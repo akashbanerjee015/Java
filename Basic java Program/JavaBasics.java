4f2b4c890887721d04a3243f513929559344dbe0
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-02-19 22:33:55 +0530

    Add files via upload

---
+++ Basic java Program/JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics{
+         public static void main(String[] args) {
+            int a= 10;
+             int b= 5;
+             System.out.print(a+b);
+         }
+     }
+// OUTPUT :- 15
+     
