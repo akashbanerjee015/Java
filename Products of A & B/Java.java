ceef5583c50ef20d928c8bc0d0c4cb96e4d0fb5a
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-02-25 02:29:02 +0530

    Add files via upload

---
+++ Products of A & B/Java.java
@@ -0,0 +1,16 @@
+//Products of A & B
+
+import java.util.*;
+ public class Java{
+     public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        int a= sc.nextInt() ;
+         int b= sc.nextInt();
+         int product= a*b;
+         System.out.println(product);
+     }
+ }
+
+//example OUTPUT :- 15 20
+//                  300
