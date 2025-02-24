84ed994a1270b2332853ee2e1b1faa342daef56c
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-02-25 02:34:31 +0530

    Add files via upload

---
+++ Area of a Circle/JavaBasics.java
@@ -0,0 +1,15 @@
+//Area of a Circle
+
+import java.util.*;
+ public class JavaBasics{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         float rad = sc.nextFloat();
+         float area = 3.14f *rad* rad ;
+         System.out.println(area);
+
+     }
+ }
+
+//example OUTPUT :- 14
+//                 615.44006
