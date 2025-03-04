a7657b2c5aa01cefae9311d18f13205ef633db99
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-03-05 03:07:14 +0530

    Add files via upload

---
+++ Print number from 1 to 10/JavaBasics.java
@@ -0,0 +1,17 @@
+//Print number from 1 to 10
+
+import java.util.*;
+ public class JavaBasics {
+
+   public static void main(String[] args) {
+     int counter = 1;
+     while (counter<= 10){
+       System.out.println(counter+" ");
+       counter++;
+     }
+     System.out.println();
+    
+   }
+}
+
+//OUTPUT :- 1 2 3 4 5 6 7 8 9 10
