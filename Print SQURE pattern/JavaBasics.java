6e7de113bb5db5c9d70287c9b67577eae3cfe743
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-03-06 03:55:20 +0530

    Add files via upload

---
+++ Print SQURE pattern/JavaBasics.java
@@ -0,0 +1,16 @@
+//Print SQURE pattern 
+
+  public class JavaBasics {
+
+    public static void main(String[] args) {
+
+     for(int lines = 1; lines<= 4; lines++){
+       System.out.println("****");
+     }
+   
+   }
+}
+//OUTPUT :- ****
+//          ****
+//          ****
+//          ****
