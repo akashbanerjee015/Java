dce2a2a3ea80c63beb951ff6dc9ad500bb474f85
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-03-07 02:43:54 +0530

    Add files via upload

---
+++ Print Reverse of a number/JavaBasics.java
@@ -0,0 +1,17 @@
+//Print Reverse of a number
+
+ public class JavaBasics {
+
+       public static void main(String[] args) {
+         int n= 10899;
+      
+         while(n>0){
+           int lastDigit =  n % 10;
+           System.out.print(lastDigit+ " ");
+           n =n / 10; 
+         }
+         System.out.println();
+    }
+}
+
+//OUTPUT :- 9 9 8 0 1
