6e47fb48a47d2182c40b0abe6de6d3d9c077a61b
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-03-11 02:49:58 +0530

    Add files via upload

---
+++ Print Star patten/JavaPattern.java
@@ -0,0 +1,18 @@
+//Print Star patten
+
+ public class JavaPattern{
+     public static void main(String[] args) {
+     for(int line=1; line<=4 ; line++){
+         //one line
+         for (int star = 1; star <=line; star++){
+             System.out.print("*");
+         }
+         System.out.println();
+     }
+   }   
+}
+
+//OUTPUT:- *
+//         **
+//         ***  
+//         ****
