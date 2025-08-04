908e14f5008f8cc14fd7791855791ff302edb8e8
Akash Banerjee <162049221+akashbanerjee015@users.noreply.github.com>
2025-08-04 23:36:47 +0530

    Add files via upload

---
+++ Factorial of a number n/JavaBasics.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+ public class JavaBasics {
+
+     public static int factorial(int n){
+         int f=1 ;
+         for(int i = 1; i<=n; i++){
+             f= f * i;
+
+         }
+         return f; //factorial of n
+
+    }
+
+     public static void main(String[] args) {
+       System.out.println( "Factorial = " + factorial(4));
+     }
+}
+// Factorial = 24 
